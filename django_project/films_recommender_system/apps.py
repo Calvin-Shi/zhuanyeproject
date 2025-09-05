7535f99d4513a63c44e32c42c41376d929c8d05e
YankeTheGreat <1706867718@qq.com>
2025-09-05 10:58:58 +0800

    为配合前端，对django_project里的文件做了些许修改

--- django_project/films_recommender_system/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FilmsRecommenderSystemConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'films_recommender_system'
--- django_project/testWeb/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestwebConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'testWeb'
