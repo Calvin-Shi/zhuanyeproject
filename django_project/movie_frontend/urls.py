75fc3e8a71bb4e07a11b9078232607d063538743
YunXiao <1139529798@qq.com>
2025-09-14 17:37:57 +0800

    完善了用户评论功能

--- django_project/movie_frontend/urls.py
+++ django_project/movie_frontend/urls.py
@@ -5,16 +5,19 @@ app_name = 'movie_frontend'
 
 urlpatterns = [
     path('accounts/signup/', views.signup, name='signup'),
-
     path('', views.home, name='home'),
     path('movies/', views.movie_list, name='movie_list'),
     path('movies/<int:movie_id>/', views.movie_detail, name='movie_detail'),
     path('recommendations/', views.recommendations, name='recommendations'),
     path('choose-favorites/', views.choose_favorites, name='choose_favorites'),
     path('movies/<int:movie_id>/toggle_favorite/', views.toggle_favorite, name='toggle_favorite'),
-
-    # 新增的 URL
     path('movies/<int:movie_id>/toggle_watchlist/', views.toggle_watchlist, name='toggle_watchlist'),
-
     path('profile/', views.profile, name='profile'),
+
+    # --- 新增评论相关URL ---
+    path('review/<int:review_id>/edit/', views.edit_review, name='edit_review'),
+    path('review/<int:review_id>/delete/', views.delete_review, name='delete_review'),
+
+    # --- 新增点赞URL ---
+    path('review/<int:review_id>/like/', views.like_review, name='like_review'),
 ]
